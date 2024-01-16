830aead76cf854b6c558752b2b5a496884f2322c
Arjun Sarao <arjunsarao727@gmail.com>
2024-01-16 09:37:17 -0500

    stuff

--- hamilton_bot/app.py
+++ hamilton_bot/app.py
@@ -1,38 +1,71 @@
+from langchain_community.vectorstores import Chroma
+from langchain_openai import OpenAI
+from langchain.chains import RetrievalQA
+from langchain_community.document_loaders.parsers.pdf import PyPDFParser
+from langchain_community.document_loaders.parsers.msword import MsWordParser
+from langchain_core.vectorstores import VectorStoreRetriever
 import streamlit as st
-import time
+from langchain_community.embeddings.sentence_transformer import (
+    SentenceTransformerEmbeddings,
+)
+import tempfile
 
+PDF: str = "application/pdf"
+DOCX: str = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+
+# Setup the DB
+vectordb = Chroma(
+    persist_directory="./chroma_db",
+    embedding_function=SentenceTransformerEmbeddings(model_name="all-MiniLM-L6-v2"),
+)
+
+
+st.set_page_config(page_title="HamiltonBot", page_icon="🤖")
 st.title("HamiltonBot")
 st.text("(Better name pending)")
 
 
 st.sidebar.title("Files")
-file = st.sidebar.file_uploader(
-    label="Upload a file", accept_multiple_files=True, type=["pdf", ".docx", ".txt"]
+files = st.sidebar.file_uploader(
+    label="Upload Documents", accept_multiple_files=True, type=["pdf", ".docx"]
 )
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-if file:
+if files:
+    for file in files:
+        file_content = file.read()
+
+        # Create a temporary file and write the content
+        with tempfile.NamedTemporaryFile(delete=False, mode='wb') as temp_file:
+            temp_file.write(file_content)
+            temp_file_path = temp_file.name
+
+            if file.type == "PDF":
+                vectordb.add_documents(documents=PyPDFParser(temp_file_path))
+            elif file.type == "DOCX":
+                vectordb.add_documents(documents=MsWordParser(temp_file_path))
+
+    qa = RetrievalQA.from_llm(
+        llm=OpenAI(api_key="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"),
+        retriever=vectordb.as_retriever(),
+    )
+
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
 
-    if prompt := st.chat_input("What is up?"):
-        st.chat_message("user").markdown(prompt)
-        st.session_state.messages.append({"role": "user", "content": prompt})
+    if message := st.chat_input("Message HamiltonBot..."):
+        st.chat_message("user").markdown(message)
+        st.session_state.messages.append({"role": "user", "content": message})
 
-        response = prompt
-        with st.spinner("Awaiting response..."):
-            # Either leave this in for a bit for "performance improvements" or just remove it lmao
-            time.sleep(2)
+        response = qa.invoke({"query": message})
+
         with st.chat_message("assistant"):
-            st.markdown(response)
+            st.markdown(response["result"])
         st.session_state.messages.append({"role": "assistant", "content": response})
 else:
     if st.session_state.messages:
         st.session_state.messages = []
-    # Make it so that if the same file is uploaded, the chat history is saved
-    # Or we can just clear it every time a new file is uploaded
-
     st.error(body="Please upload a pdf/docx/txt file to get started!", icon="🚨")
--- readme.md
+++ readme.md
@@ -1,3 +1,24 @@
 # HamiltonBot
 
-This is the repository for the HamiltonBot app. It is a `streamlit` app that utilizes `langchain` and `chromadb` to create a chat-with-pdf/docx-app. This is fairly standat
+This is the repository for the HamiltonBot app. It is a `streamlit` app that utilizes `langchain` and `chromadb` to create a chat-with-pdf/docx-app. This is fairly standard.
+
+```sh
+cd .\hamilton_bot\
+python -m streamlit run app.py
+```
+
+## Roadmap
+
+~~1. Get frontend done using `streamlit`~~
+2. Get generic responses from OpenAI
+3. Create Tokenization and Embedding for uploaded documents
+4. Pass entire document to OpenAI
+5. Upload Embeddings to vectorstore
+6. Make similarity search through vectorstore and pass into OpenAI
+7. Make OpenAI from Azure instead
+
+
+## File Handling
+
+1. We get `List[UploadedFile]` from `st.file_uploader`. This object has a `.type` attribute that we will have to use with langchain in order to convert it into strings.
+2. https://medium.com/@onkarmishra/using-langchain-for-question-answering-on-own-data-3af0a82789ed

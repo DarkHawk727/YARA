73019f3a77c681cfced0f6ef3c258d048b4cbb0f
Arjun Sarao <arjunsarao727@gmail.com>
2024-01-17 12:04:24 -0500

    stuff

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
 import streamlit as st
-from typing import List, Optional
+import time
 
 st.set_page_config(page_title="HamiltonBot", page_icon="🤖")
-st.title("HamiltonBot")
+st.title("🤖 HamiltonBot")
 st.text("(Better name pending)")
 
 
 st.sidebar.title("Files")
-files = st.sidebar.file_uploader(
-    label="Upload Documents", accept_multiple_files=True, type=["pdf", ".docx"]
-)
+
+files = st.sidebar.file_uploader(label="Upload Documents", accept_multiple_files=True, type=["pdf", ".docx"])
+
+
+st.sidebar.subheader("Previous Conversations")
+st.sidebar.selectbox("Select a conversation", ("Conversation 1", "Conversation 2"))
 
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
 if files:
+
     st.header("Chat")
     for message in st.session_state.messages:
         with st.chat_message(message["role"]):
@@ -25,10 +29,16 @@ if files:
         st.session_state.messages.append({"role": "user", "content": prompt})
 
         response: str = f">\"CITATION\"\n\n {prompt}"
+        with st.status("Awaiting Response..."):
+            st.write("Creating Multiple Queries...")
+            time.sleep(2)
+            st.write("Retrieving Chunks...")
+            time.sleep(1)
+            st.write("Sending to LLM...")
+            time.sleep(1)
         with st.chat_message("assistant"):
             st.markdown(response)
         st.session_state.messages.append({"role": "assistant", "content": response})
-
 
 else:
     if st.session_state.messages:

c01980e219f9a572fb314fc3c7548a618ccca5f9
Arjun Sarao <arjunsarao727@gmail.com>
2024-01-24 11:39:07 -0500

    trying to use unstructured

--- RAG-implementation.py
+++ RAG-implementation.py
@@ -1,19 +1,21 @@
+from langchain_community.document_loaders import UnstructuredFileIOLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community.vectorstores.utils import filter_complex_metadata
+from langchain.docstore.document import Document
 from langchain_openai import ChatOpenAI
 from langchain_openai.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma
-from langchain_community.document_loaders import UnstructuredAPIFileIOLoader
-from unstructured.cleaners.core import clean_extra_whitespace
 from langchain.retrievers.multi_query import MultiQueryRetriever
-from langchain_experimental.text_splitter import SemanticChunker
-from langchain import Document
-
-from typing import List
 
 import random
 
-FILE_PATH: str = "SAMPLE"
+from typing import List
+
+from unstructured.cleaners.core import clean_extra_whitespace
+
+
+FILE_PATH: str = "pedpolicies-storm-drainage-policy.pdf"
 OPENAI_API_KEY: str = "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"
-UNSTRUCTURED_API_KEY: str = "XXXXXXXXXXX"
+
 questions: List[str] = [
     "What are the primary goals and objectives for stormwater and drainage management within the City of Hamilton, as outlined in the document?",
     "Can you describe the legislative framework that influences the stormwater management practices in the City of Hamilton?",
@@ -29,39 +31,30 @@ questions: List[str] = [
 
 QUESTION: str = random.choice(questions)
 
-with open(file=FILE_PATH, mode="r") as f:
-    loader = UnstructuredAPIFileIOLoader(
+with open(file=FILE_PATH, mode="rb") as f:
+    loader = UnstructuredFileIOLoader(
         file=f,
         mode="elements",
         show_progress_bar=True,
-        api_key=UNSTRUCTURED_API_KEY,
         post_processors=[clean_extra_whitespace],
     )
-docs: List[Document] = loader.load()
-
-embedding = OpenAIEmbeddings(
-    api_key=OPENAI_API_KEY,
-    openai_api_type="davinci",
-)
-
-text_splitter = SemanticChunker(embedding=embedding)
-chunks: List[str] = text_splitter.split_documents(docs)
+    docs: List[Document] = loader.load()
 
 
 vectordb = Chroma(
     persist_directory="./chroma_db",
-).from_documents(documents=chunks, embedding=embedding)
+).from_documents(
+    documents=filter_complex_metadata(documents=docs),
+    embedding=OpenAIEmbeddings(
+        api_key=OPENAI_API_KEY,
+        openai_api_type="davinci",
+    ),
+)
 
-
-retriever_from_llm = MultiQueryRetriever.from_llm(
+retriever_from_llm: MultiQueryRetriever = MultiQueryRetriever.from_llm(
     retriever=vectordb.as_retriever(search_type="mmr", search_kwargs={"k": 3}),
     llm=ChatOpenAI(temperature=0, api_key=OPENAI_API_KEY),
 )
 
 unique_docs: List[Document] = retriever_from_llm.get_relevant_documents(query=QUESTION)
 
-response = retriever_from_llm.llm.generate_response(
-    query=QUESTION, documents=unique_docs, max_tokens=100
-)
-
-print(response)
